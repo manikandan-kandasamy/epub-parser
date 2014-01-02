25d6d357e4320bb8c8d7855b99437b2b29237b06
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-01-02 23:56:04 +0900

    [REFACTORING]Extract searcher class from ContentDocument::XHTML#search

--- lib/epub/content_document/xhtml.rb
+++ lib/epub/content_document/xhtml.rb
@@ -42,72 +42,61 @@ module EPUB
         @nokogiri ||= Nokogiri.XML(raw_document)
       end
 
-      # @param query [String] search word
-      # @param element [Nokogiri::XML::Node, nil]
-      # @param steps [Array<Hash>]
-      # @return [Array<Array<CFI::Step>>] start points of found search word
-      def search(query, element=nil, steps=[])
-        unless element
-          element = Nokogiri.XML(raw_document) {|config|
-            config.options = Nokogiri::XML::ParseOptions::NOENT | Nokogiri::XML::ParseOptions::NOCDATA
-          }
-        end
-        result = []
-        text_index = -1
-        elem_index = 0
-        stepping_over_subquery = nil
-        stepping_over_offset = nil
-        element.children.each do |child|
-          case child.type
-          when Nokogiri::XML::Node::TEXT_NODE
-            text_index = elem_index + 1
-            pos = 0
-            content = child.content
-            while pos
-              pos = content.index(query, pos)
-              if pos
-                result_steps = steps.map {|step_info| CFI::Step.new(step_info)}
-                result_steps << CFI::Step.new(index: text_index, character_offset: pos)
-                result << result_steps
-                pos += 1
-              end
-            end
+      def search(query)
+        doc = Nokogiri.XML(raw_document) {|config|
+          config.options = Nokogiri::XML::ParseOptions::NOENT | Nokogiri::XML::ParseOptions::NOCDATA
+        }
+        Searcher.search(query, doc)
+      end
 
-            sublength = query.length - 1
-            until (subquery = query[0, sublength]).to_s.empty?
-              subcontent = content[-sublength..-1]
-              if subquery == subcontent
-                stepping_over_subquery = query[sublength..-1]
-                stepping_over_offset = content.length - sublength
-                break
-              end
-              sublength -= 1
-            end
-          when Nokogiri::XML::Node::ELEMENT_NODE
-            elem_index += 2
-            if child.node_name == 'img' and child['alt'].index(query)
-              result_steps = steps.map {|step_info| CFI::Step.new(step_info)}
-              result_steps << CFI::Step.new(element: 'img', index: elem_index, id: child['id'])
-              result << result_steps
-            end
-
-            if stepping_over_subquery and STEPPING_OVER_ELEMENTS.include? child.node_name
-              child_result = search(stepping_over_subquery, child, [])
-              unless child_result.empty?
-                result_steps = steps.map {|step_info| CFI::Step.new(step_info)}
-                result_steps << CFI::Step.new(character_offset: stepping_over_offset, index: text_index)
-                result << result_steps
-              end
-            else
-              next_steps = steps.dup
-              next_steps << {:element => child.node_name, :index => elem_index, :id => child['id']}
-              result.concat search(query, child, next_steps)
-            end
-            stepping_over_subquery = nil
-            stepping_over_offset = nil
+      class Searcher
+        class << self
+          def search(query, doc)
+            element = doc.respond_to?(:root) ? doc.root : doc
+            new(query).search(element)
           end
         end
-        result
+
+        def initialize(query)
+          @query = query
+        end
+
+        def search(element, steps=nil)
+          steps ||= [{:element => element.node_name, :index => 2, :id => element['id']}]
+          results = []
+          text_index = -1
+          elem_index = 0
+          element.children.each do |node|
+            case node.type
+            when Nokogiri::XML::Node::TEXT_NODE
+              text_index = elem_index + 1
+              pos = 0
+              content = node.content
+              while pos
+                pos = content.index(@query, pos)
+                if pos
+                  result_steps = steps.map {|step| CFI::Step.new(step)}
+                  result_steps << CFI::Step.new(index: text_index, character_offset: pos)
+                  results << result_steps
+                  pos += 1
+                end
+              end
+            when Nokogiri::XML::Node::ELEMENT_NODE
+              elem_index += 2
+
+              if node.node_name == 'img' and node['alt'].index(@query)
+                result_steps = steps.map {|step| CFI::Step.new(step)}
+                result_steps << CFI::Step.new(element: 'img', index: elem_index, id: node['id'])
+                results << result_steps
+              end
+
+              next_steps = steps.dup
+              next_steps << {:element => node.node_name, :index => elem_index, :id => node['id']}
+              results.concat search(node, next_steps)
+            end
+          end
+          results
+        end
       end
     end
   end
