44f1433edb61fc0b22706f4ddecc759517368f86
Manikandan Kandasamy <kmani2k5@gmail.com>
2015-05-09 15:06:27 +0530

    epub module renamed to epub3

--- lib/epub3/ocf.rb
+++ lib/epub3/ocf.rb
@@ -1,7 +1,7 @@
 module EPUB3
   class OCF
     MODULES = %w[container encryption manifest metadata rights signatures]
-    MODULES.each {|m| require "epub/ocf/#{m}"}
+    MODULES.each {|m| require "epub3/ocf/#{m}"}
 
     attr_accessor :book, *MODULES
   end
